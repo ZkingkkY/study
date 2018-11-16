42607f3cf4c76ba2359787af3a2aea5314f8c176
coder-pig <33276861+ZkingkkY@users.noreply.github.com>
2018-11-16 17:47:07 +0800

    2018.10.30 新增version文件

--- Test3.php
+++ Test3.php
@@ -1,4 +1,3 @@
-
 <?php
 
 /*
@@ -75,11 +74,139 @@ function dg( $n ){
 }
 */
 
-
-if(copy('D:\a.txt','D:\b.txt')){
+/*
+if(copy('D:\a.txt','D:\b.txt')){//绝对路径
     echo '复制成功';
 }else{
     echo '复制失败';
 }
+*/
+
+/*
+//include_once 'version.php';
+//include'version.php';
+require 'version.php';
+require_once 'version.php';
+Demo();
+Test();
+*/
+
+
+/*
+//定义一下时区常量，以后你可以放到配置文件里
+define('TIME_ZONE','Asia/shanghai');
+
+//执行函数
+date_default_timezone_set(TIME_ZONE);
+echo date('Y-m-d H:i:s').'<br>'."\n";
+
+$mytime = getdate();
+echo "年 :".$mytime['year']."\n";
+echo "月 :".$mytime['mon']."\n";
+echo "日 :".$mytime['mday']."\n";
+echo "时 :".$mytime['hours']."\n";
+echo "分 :".$mytime['minutes']."\n";
+echo "秒 :".$mytime['seconds']."\n";
+echo "一个小时中的第几钟 :".$mytime['minutes']."\n";
+echo "这是一分钟的第几秒 :".$mytime['seconds']."\n";
+echo "星期名称 :".$mytime['weekday']."\n";
+echo "月份名称 :".$mytime['month']."\n";
+echo "时间戳   :".$mytime[0]."\n";
+
+//检测时间函数
+var_dump(checkdate(12, 31, 2018));
+var_dump(checkdate(2, 29, 2011));
+
+echo date("m-d-Y h:m:s")."\n";
+echo date("m-d-Y h:m:s",mktime(10,15,35,date("m"),date("d"),date("Y")))."\n";
+echo date("m-d-Y h:m:s",mktime(10,15,35,date("m"),date("d")-30,date("Y")))."\n";
+*/
+
+/*
+$area = array(
+
+    'china' => array(
+
+        '上海',
+        '湖北',
+        '天津',
+        '北京' => array(
+            'hd' => '海淀',
+            '朝阳',
+            '房山',
+            'cp' => '昌平',
+        ),
+
+        '广东' => array(
+            '深圳',
+            '广州',
+            '佛山',
+            'dg' => '东莞',
+
+        ),
+
+    ),
+
+
+    'usa' => array(
+
+        '华盛顿',
+        '旧金山',
+        '纽约' => array(
+            '曼哈顿区',
+            '皇后区',
+            '布鲁克林区',
+        ),
+
+    ),
+);
+
+
+//读取华盛顿
+echo $area['usa']['0'];
+
+//读取：布鲁克林
+echo $area['usa']['纽约'][2];
+
+
+//读取：昌平
+echo $area['china']['北京']['cp'];
+
+//修改cp下标的值改为：西城区
+
+$area['china']['北京']['cp'] = '西城区';
+
+//输出看看原来昌平的值是否发生了变化
+echo $area['china']['北京']['cp'];
+*/
+
+$arr=array(
+    '教学部'=>array(
+        array('李某','18','人妖'),
+        array('高某','20','男'),
+        array('张某','21','妖人'),
+    ),
+    '宣传部'=>array(
+        array('李某','18','人妖'),
+        array('高某','20','男'),
+        array('张某','21','妖人'),
+    ),
+    '财务部'=>array(
+        array('李某','18','人妖'),
+        array('高某','20','男'),
+        array('张某','21','妖人'),
+    ),
+);
+
+
+foreach ($arr as $value){
+
+
+    foreach ('财务部' as $k => $v){
+        echo $k.'-----------------'.$v;
+    }
+}
 ?>
+
+
 
--- version.php
+++ version.php
@@ -5,5 +5,13 @@
  * Date: 2018/10/30
  * Time: 17:25
  */
-//define('AUTH',false);
+define('AUTH',false);
+
+function Demo(){
+    echo '我是Demo'.'<br>';
+}
+
+function Test(){
+    echo '我是Test'.'<br>';
+}
 ?>
